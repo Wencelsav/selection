b43f53b76f5ad3ef175665cce96ec3c4d1e8da4a
Wencelsav <31541176+Wencelsav@users.noreply.github.com>
2017-11-10 16:21:58 +0300

    Update bubble_sort.example.cpp

--- bubble_sort.example.cpp
+++ bubble_sort.example.cpp
@@ -1,29 +1,31 @@
+
 #include <iostream>
 #include <iomanip>
+#include <sstream>
 using namespace std;
- 
 
 
- 
+
+
 void bubbleSort(int* a, int n)
 {
- int temp = 0; 
- bool exit = false; 
- 
- while (!exit) 
- {
-  exit = true;
-  for (int int_counter = 0; int_counter < (n - 1); int_counter++)
-   
-    if (a[int_counter] > a[int_counter + 1]) 
-    {
-    
-     temp = a[int_counter];
-     a[int_counter] = a[int_counter + 1];
-     a[int_counter + 1] = temp;
-     exit = false; 
-    }
- }
+	int temp = 0;
+	bool exit = false;
+
+	while (!exit)
+	{
+		exit = true;
+		for (int int_counter = 0; int_counter < (n - 1); int_counter++)
+
+			if (a[int_counter] > a[int_counter + 1])
+			{
+
+				temp = a[int_counter];
+				a[int_counter] = a[int_counter + 1];
+				a[int_counter + 1] = temp;
+				exit = false;
+			}
+	}
 }
 bool read_numbers(int *a, int n)
 {
@@ -31,8 +33,10 @@ bool read_numbers(int *a, int n)
 	getline(cin, string);
 	istringstream stream(string);
 	bool success = true;
-	for (int i = 0; i < n; ++i) {
-		if (!(stream >> a[i])) {
+	for (int i = 0; i < n; ++i) 
+	{
+		if (!(stream >> a[i])) 
+		{
 			success = false;
 			break;
 		}
@@ -47,15 +51,17 @@ int main() {
 	cin >> n;
 	cin.get();
 	a = new int[n];
-	if (read_numbers(a, n)) {
+	if (read_numbers(a, n))
+	{
 		bubbleSort(a, n);
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < n; i++) 
+		{
 			cout << a[i] << " ";
 		}
 	}
 	else {
 		cout << "An error has occured while reading input data.";
 	}
-	
+	delete[] a;
 	return 0;
 }
