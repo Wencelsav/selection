cb55b1343d2b48e08bc6f3dd4edf9d7f622c1442
Wencelsav <31541176+Wencelsav@users.noreply.github.com>
2017-11-10 16:32:08 +0300

    Update bubble_sort.example.cpp

--- bubble_sort.example.cpp
+++ bubble_sort.example.cpp
@@ -9,20 +9,20 @@ using namespace std;
 
 void bubbleSort(int* a, int n)
 {
-	int temp = 0;
+	int swap = 0;
 	bool exit = false;
 
 	while (!exit)
 	{
 		exit = true;
-		for (int int_counter = 0; int_counter < (n - 1); int_counter++)
+		for (int i = 0; i < (n - 1); i++)
 
-			if (a[int_counter] > a[int_counter + 1])
+			if (a[i] > a[i + 1])
 			{
 
-				temp = a[int_counter];
-				a[int_counter] = a[int_counter + 1];
-				a[int_counter + 1] = temp;
+				swap = a[i];
+				a[i] = a[i + 1];
+				a[i + 1] = swap;
 				exit = false;
 			}
 	}
@@ -45,7 +45,8 @@ bool read_numbers(int *a, int n)
 	return success;
 }
 
-int main() {
+int main() 
+{
 	int n;
 	int *a;
 	cin >> n;
