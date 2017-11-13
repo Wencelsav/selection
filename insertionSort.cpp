c7861b7bf5d49c374d21b71a9a6824e371d4434d
Wencelsav <31541176+Wencelsav@users.noreply.github.com>
2017-11-13 15:40:11 +0300

    Update insertionSort.cpp

--- insertionSort.cpp
+++ insertionSort.cpp
@@ -5,33 +5,22 @@ using namespace std;
 void insertionSort(int *, int); 
  
 int main(int argc, char* argv[])
-{
-    
-    
-    
+{    
     int size_array; 
     cin >> size_array;
     int counter;
     int sorted_array[size_array];
-    
     for (counter = 0; counter < size_array; counter++)
     {
        cin>> sorted_array[counter] ;
        if ( size_array>counter)
        { cout<<"An error has occured while reading input data.";}
-    
     }
-    
-    
- 
     insertionSort(sorted_array, size_array); 
- 
     for (int counter = 0; counter < size_array; counter++)
     { 
-        cout << setw(1) << sorted_array[counter] << "  ";
-       
-    }
-     
+       cout << setw(1) << sorted_array[counter] << "  ";      
+    }     
     return 0;
 }
  
