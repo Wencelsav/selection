f11fa57c8401f5d77da7835562cdb5ec8c5d2e01
Wencelsav <31541176+Wencelsav@users.noreply.github.com>
2017-12-17 13:17:51 +0300

    Update heap_sort.001.cpp

--- heap_sort.001.cpp
+++ heap_sort.001.cpp
@@ -2,73 +2,75 @@
 #include <stdlib.h>
 #include <iostream>
 #include <sstream>
+
 using namespace std;
 
-bool read(int * a, unsigned int size) {
-    string stroka;
-    getline(cin, stroka);
-    istringstream stream(stroka);
-    for (unsigned int i = 0; i < size; ++i) {
-        if (!(stream >> a[i])) {
-            return false;
-        }
-    }
-    return true;
+bool read(int * a, int n) {
+	string mas;
+	int array_count = 0;
+	getline(cin, mas);
+	istringstream stream(mas);
+	for (int i = 0; i < n; i++)
+	{
+		if (stream >> a[i])
+		{
+			array_count++;
+		}
+	}
+	if (array_count == n && stream.eof()) return true;
+	else return false;
 }
-void siftDown(int *numbers, int root, int bottom)
-{
-    int maxChild;
-    int done = 0; 
-    while ((root * 2 <= bottom) && (!done))
-    {
-        if (root * 2 == bottom)    
-            maxChild = root * 2;
-        else if (numbers[root * 2] > numbers[root * 2 + 1])
-            maxChild = root * 2;
-        else
-            maxChild = root * 2 + 1;
-        
-        if (numbers[root] < numbers[maxChild])
-        {
-            int temp = numbers[root]; 
-            numbers[root] = numbers[maxChild];
-            numbers[maxChild] = temp;
-            root = maxChild;
-        }
-        else 
-            done = 1;
-    }
-}
-void heapSort(int *numbers, int array_size)
-{
-    for (int i = (array_size / 2) - 1; i >= 0; i--)
-        siftDown(numbers, i, array_size);
-    for (int i = array_size - 1; i >= 1; i--)
-    {
-        int temp = numbers[0];
-        numbers[0] = numbers[i];
-        numbers[i] = temp;
-        siftDown(numbers, 0, i - 1);
-    }
+void heapSort(int *a, int n) {
+
+	int sh = 0;
+	bool b;
+	do {
+		b = false;
+		for (int i = 0; i < n; ++i) {
+			if (i * 2 + 2 + sh < n) {
+				if ((a[i + sh] > /*<*/ a[i * 2 + 1 + sh]) || (a[i + sh] > /*<*/ a[i * 2 + 2 + sh])) {
+					if (a[i * 2 + 1 + sh] < /*>*/ a[i * 2 + 2 + sh]) {
+						swap(a[i + sh], a[i * 2 + 1 + sh]);
+						b = true;
+					}
+					else if (a[i * 2 + 2 + sh] < /*>*/ a[i * 2 + 1 + sh]) {
+						swap(a[i + sh], a[i * 2 + 2 + sh]);
+						b = true;
+					}
+				}
+				if (a[i * 2 + 2 + sh] < /*>*/ a[i * 2 + 1 + sh]) {
+					swap(a[i * 2 + 1 + sh], a[i * 2 + 2 + sh]);
+					b = true;
+				}
+			}
+			else if (i * 2 + 1 + sh < n) {
+				if (a[i + sh] > /*<*/ a[i * 2 + 1 + sh]) {
+					swap(a[i + sh], a[i * 2 + 1 + sh]);
+					b = true;
+				}
+			}
+		}
+		if (!b)
+			++sh;
+	} while (sh + 2 < n);
 }
 int main()
 {
-
-    int size = 0;
-    if ((cin >> size) && (size>1)) {
-        string s;
-        getline(cin, s);
-        istringstream stream(s);
-        int * a = new int[size];
-        if (read(a, size)) {
-            heapSort(a, size);;
-            for (int i = 0; i < size; ++i) {
-                cout << a[i] << " ";
-            }
-        }
-        else cout << "An error has occured while reading input data." << endl;
-        delete[] a;
-    }
-    else cout << "An error has occured while reading input data." << endl;
-    return 0;
+	int size = 0;
+	if ((cin >> size) && (size>1)) {
+		string s;
+		getline(cin, s);
+		istringstream stream(s);
+		int * a = new int[size];
+		if (read(a, size)) {
+			heapSort(a, size);
+			for (int i = 0; i < size; ++i) {
+				cout << a[i] << " ";
+			}
+		}
+		else cout << "An error has occured while reading input data." << endl;
+		delete[] a;
+	}
+	else cout << "An error has occured while reading input data." << endl;
+	return 0;
 }
